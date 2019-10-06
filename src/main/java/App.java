a4e4828529e45104867ef40a4f47f3ce2da1c0aa
tuyetngo1 <tuyetngo136@gmail.com>
2019-10-06 14:56:46 +0100

    random foodjoke added - homepage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,34 @@
 import dao.Sql2oFoodDAO;
+import kong.unirest.HttpRequestWithBody;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import org.json.JSONObject;
 import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
+import kong.unirest.Unirest;
 
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        port(9000);
 
         String connectionString = "jdbc:postgresql://localhost:5432/nutrisi";
         Sql2o sql2o = new Sql2o(connectionString, "student", "");
         Sql2oFoodDAO foodDAO = new Sql2oFoodDAO(sql2o);
 
-        get("/", (request, response) -> {
+        get("/", (request, response)  -> {
             Map<String, Object> model = new HashMap<>();
+            String joke = Unirest.get("https://api.spoonacular.com/food/jokes/random?apiKey=5d9050c0cf864963aad55a329d51e429")
+                    .asString()
+                    .getBody();
+            JSONObject jokeJson = new JSONObject(joke);
+            String jokeText = jokeJson.getString("text");
+            model.put("joke", jokeText);
             return new ModelAndView(model, "templates/index.vtl");
         }, new VelocityTemplateEngine());
 
@@ -34,5 +46,6 @@ public class App {
             model.put("foodDAO", foodDAO);
             return new ModelAndView(model, "templates/searchresults.vtl");
         }, new VelocityTemplateEngine());
+
     }
 }
